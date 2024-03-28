16487600fc041dffb03bc12f08509711202835bc
Denys Kovalyov <kovalovdenis@gmail.com>
2024-03-28 16:02:30 +0200

    Initialize basic project structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # activities-time-tracker
+
 Web application to track time spent on various activities
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | ATT',
+    default: 'Activities Time Tracker',
+  },
+  description: 'Web application to track time spent on various activities.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
