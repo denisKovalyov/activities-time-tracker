b37d35b18e6cf531ec67c60e9c2ccd99d3c459e0
Denys Kovalyov <kovalovdenis@gmail.com>
2024-10-06 00:44:07 +0300

    Improve UX for activity item swiping

--- src/ui/dashboard/activities-list/activity-item.tsx
+++ src/ui/dashboard/activities-list/activity-item.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
 import { useSwipeable } from 'react-swipeable';
 import { X, NotePencil } from '@phosphor-icons/react';
@@ -25,6 +25,8 @@ export const ActivityItem = ({
   onSwipe: (id: string) => void;
   onCancelSwipe: () => void;
 }) => {
+  const [shift, setShift] = useState(0);
+  const [isSwiping, setIsSwiping] = useState(false);
   const ref = useRef(null);
 
   const {
@@ -37,24 +39,47 @@ export const ActivityItem = ({
 
   const handlers = useSwipeable({
     onSwipedLeft: () => onSwipe(id),
-    onSwipedRight: () => onCancelSwipe(),
-    onSwiping: (e) => console.log(e),
+    onSwipedRight: () => {
+      if (swiped) onCancelSwipe();
+    },
+    onSwiping: (e) => {
+      if (!isSwiping) setIsSwiping(true);
+
+      if (e.dir === 'Left' && Math.abs(shift) <= 110) setShift(e.deltaX < -110 ? -110 : e.deltaX);
+      if (e.dir === 'Right') setShift((shift) => {
+        const delta = shift + e.absX;
+        return delta > 0 ? 0 : delta;
+      });
+    },
+    onSwiped: () => {
+      if (isSwiping) setIsSwiping(false);
+      if (!swiped) setShift(0);
+    }
   });
 
   const totalTimeSpent = useCalculateTimeValues(timeSpent);
 
   const handleClickOutsideButtons = () => {
+    setShift(0);
     if (swiped) onCancelSwipe();
   };
 
   useOnClickOutside(ref, handleClickOutsideButtons, 'touchstart');
 
+  const buttonWidth = Math.floor(Math.abs(shift) / 2);
+
   return (
     <div className="relative">
       <Card
-        className={cn('w-full flex justify-between items-center mb-4 p-4 transition-transform duration-200 ease-in-out', {
-          '-translate-x-[110px] rounded-r-none': swiped,
+        className={cn('w-full flex justify-between items-center mb-4 p-4 will-change-transform transition-transform duration-200 ease-in-out', {
+          '-translate-x-[110px] rounded-r-none duration-200': swiped,
+          'duration-0': isSwiping,
+          'rounded-r-none': shift !== 0,
         })}
+        style={isSwiping
+          ? { 'transform': `translateX(${shift}px)`}
+          : undefined
+        }
         {...handlers}
       >
         <div className="flex-none flex items-center max-w-[70%] overflow-hidden">
@@ -80,22 +105,34 @@ export const ActivityItem = ({
         <Button
           className={cn(buttonClassNames, {
             'w-auto': swiped,
-            'px-0 border-none': !swiped,
+            'px-0': !swiped,
+            'border-none': shift === 0,
+            'duration-0': isSwiping
           })}
+          style={isSwiping
+            ? { 'width': `${buttonWidth}px` }
+            : undefined
+          }
           variant="warning"
           onClick={() => console.log('edit')}
         >
-          <NotePencil size="30" />
+          {(buttonWidth >= 30 || swiped) && <NotePencil size="30" />}
         </Button>
         <Button
           className={cn(`${buttonClassNames} rounded-r-md`, {
             'w-auto': swiped,
-            'px-0 border-none': !swiped,
+            'px-0': !swiped,
+            'border-none': shift === 0,
+            'duration-0': isSwiping
           })}
+          style={isSwiping
+            ? { 'width': `${buttonWidth}px` }
+            : undefined
+          }
           variant="destructive"
           onClick={() => console.log('delete')}
         >
-          <X size="30" />
+          {(buttonWidth >= 30 || swiped) && <X size="30" />}
         </Button>
       </div>
     </div>
